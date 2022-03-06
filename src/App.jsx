7aefd935b7bee667a1c9d2ac462bd4f108db50a1
MartinSugasti <martinsugasti@gmail.com>
2022-03-06 10:35:26 +0100

    Creates React app and adds bootstrap, fontawesome, eslint and prettier

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App bg-dark">
+      <p className="text-light">Working</p>
+      <i className="fas fa-user mr-1 text-light" />
+    </div>
+  );
+}
+
+export default App;
