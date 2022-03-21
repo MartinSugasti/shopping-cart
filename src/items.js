69a693ba5fab39b8e7540859cf6cc98ffb225f40
Martin Sugasti <56696387+MartinSugasti@users.noreply.github.com>
2022-03-21 23:42:50 +0100

    Store cart in local storage (#6)

--- src/items.js
+++ src/items.js
@@ -1,145 +1,143 @@
-import uniqid from 'uniqid';
-
 const items = [
   {
     name: 'Raqueta de Tenis Adulto Head Graphene 360+ Radical MP (300 GR)',
     brand: 'Head',
     price: '200',
     category: 'rackets',
-    id: 23
+    id: 0
   },
   {
     name: 'Raqueta de Tenis Babolat Pure Aero Adulto Amarillo (300GR)',
     brand: 'Babolat',
     price: '180',
     category: 'rackets',
-    id: uniqid()
+    id: 1
   },
   {
     name: 'Raqueta de Tenis Babolat Pure Drive Team Adulto (285GR)',
     brand: 'Babolat',
     price: '170',
     category: 'rackets',
-    id: uniqid()
+    id: 2
   },
   {
     name: 'Raqueta de tenis adulto BLADE 100L V8.0 Verde / Cobre sin encordar',
     brand: 'Wilson',
     price: '210',
     category: 'rackets',
-    id: uniqid()
+    id: 3
   },
   {
     name: 'Raqueta de Tenis Wilson Pro Staff 97LS V13.0 Negro (290 GR)',
     brand: 'Wilson',
     price: '200',
     category: 'rackets',
-    id: uniqid()
+    id: 4
   },
   {
     name: 'PELOTAS DE TENIS ARTENGO TB160 x3 AMARILLO',
     brand: 'Artengo',
     price: '3',
     category: 'balls',
-    id: uniqid()
+    id: 5
   },
   {
     name: 'PELOTA DE TENIS TB920 *4 POLIVALENTE BIPACK AMARILLO',
     brand: 'Artengo',
     price: '12',
     category: 'balls',
-    id: uniqid()
+    id: 6
   },
   {
     name: 'PELOTAS DE TENIS HEAD TOUR x4 BIPACK AMARILLO SPEED',
     brand: 'Head',
     price: '13',
     category: 'balls',
-    id: 42
+    id: 7
   },
   {
     name: 'CAJÓN PELOTAS TENIS ARTENGO TB930 18 BOTES x4 AMARILLO',
     brand: 'Artengo',
     price: '100',
     category: 'balls',
-    id: uniqid()
+    id: 8
   },
   {
     name: 'PELOTAS DE TENIS HEAD PRO x4 AMARILLO',
     brand: 'Head',
     price: '6',
     category: 'balls',
-    id: uniqid()
+    id: 9
   },
   {
     name: 'Zapatillas de Tenis Artengo TS500 Multi Court Hombre Naranja',
     brand: 'Artengo',
     price: '40',
     category: 'shoes',
-    id: uniqid()
+    id: 10
   },
   {
     name: 'Zapatillas de Tenis Asics Dedicate Hombre Blanco',
     brand: 'Asics',
     price: '65',
     category: 'shoes',
-    id: uniqid()
+    id: 11
   },
   {
     name: 'Zapatillas de Tenis Adidas Court Jam Hombre Negras Tierra Batida',
     brand: 'Adidas',
     price: '65',
     category: 'shoes',
-    id: uniqid()
+    id: 12
   },
   {
     name: 'ZAPATILLAS DE TENIS BABOLAT JET TERE MULTI COURT HOMBRE NEGRO GRIS',
     brand: 'Babolat',
     price: '80',
     category: 'shoes',
-    id: 78
+    id: 13
   },
   {
     name: 'OVERGRIP DE TENIS ARTENGO ABSORBENTE AZUL LOTE DE 3',
     brand: 'Artengo',
     price: '5',
     category: 'accessories',
-    id: uniqid()
+    id: 14
   },
   {
     name: 'BOBINA CORDAJE DE TENIS MONOFILAMENTO PENTAGONAL TA 930 SPIN 1,3 mm NEGRO 200 m',
     brand: 'Artengo',
     price: '70',
     category: 'accessories',
-    id: uniqid()
+    id: 15
   },
   {
     name: 'BOBINA CORDAJE DE TENIS MONOFILAMENTO SOLINCO TOURBITE GRIS 1,25 mm 200 m',
     brand: 'Solinco',
     price: '120',
     category: 'accessories',
-    id: uniqid()
+    id: 16
   },
   {
     name: 'Raquetero Tenis ARTENGO 530 L NEGRO NARANJA',
     brand: 'Artengo',
     price: '45',
     category: 'accessories',
-    id: uniqid()
+    id: 17
   },
   {
     name: 'RAQUETERO Artengo 9R TEAM negro azul',
     brand: 'Artengo',
     price: '35',
     category: 'accessories',
-    id: uniqid()
+    id: 18
   },
   {
     name: 'CESTA PARA PELOTAS TENIS',
     brand: 'Artengo',
     price: '25',
     category: 'accessories',
-    id: uniqid()
+    id: 19
   }
 ]
 
